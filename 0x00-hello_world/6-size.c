2e1b197abe8969d71aace38ebc5625f13c607e18
Emmywalk <femi7566@gmail.com>
2023-03-16 13:33:59 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int main(void)
 	long long int d;
 	float f;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)
+printf("size of a char: %lu byte(s)\n", (unsigned long)
 sizeof(a));
 printf("size of an int: %lu byte(s)\n", (unsigned long)
 sizeof(b));
