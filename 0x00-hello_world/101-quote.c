e84c106f91441adefca6d8d7902cf5454773381a
Emmywalk <femi7566@gmail.com>
2023-03-17 01:23:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A c program that print a line to the standard error
- * Return: 1 (Success)
+ * main - Entry point of the program
+ *
+ * Description: Prints a message to the standard error
+ * Return: Always 1 (Error occurred)
  */
+
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = sizeof(*msg);
+	ssize_t written = write(STDERR_FILENO, msg, len - 1);
+
+	if (written == -1)
+		return (0);
+
+	return (1);
 }
