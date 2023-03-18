6bc4e2f6fb371e3ef5a6c50b2e117970a19ce4e8
Emmywalk <femi7566@gmail.com>
2023-03-17 18:48:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,12 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints a message to the standard error
- * Return: Always 1 (Error occurred)
+ * Return: Always 1 (Succes)
  */
 
 int main(void)
 {
-	putf("and that piece of art is useful\" - Dora Kor
+	fput("and that piece of art is useful\" - Dora Kor
 par, 2015-10-19\n");
 return (1);
 }
